f55dd4f2d1ebb5adff03ad864457d07b022a3daa
girichmd@mer.ci.nsu.ru <girichmd@mer.ci.nsu.ru>
2023-09-26 15:34:52 +0700

    Create menu

--- CourseWork(SIAOD)/CourseWork(SIAOD).cpp
+++ CourseWork(SIAOD)/CourseWork(SIAOD).cpp
@@ -1,5 +1,6 @@
 ﻿//4 2 3 4 3
 #include <iostream>
+#include <cstdlib>
 #include <fstream>
 using namespace std;
 
@@ -13,27 +14,127 @@ struct record
 
 };
 
+void PrintHeader()
+{
+    cout << "Full name"
+        << "\t\t\t\t" << "Street"
+        << "\t\t" << "House number"
+        << "\t" << "Apartment number"
+        << "\t" << "Date of settle"
+        << endl;
+    cout << endl;
+}
+
+void PrintBody(record*& pl, int& page)
+{
+    for (int i = page * 20; i < page * 20 + 20; i++)
+    {
+        cout << i + 1 << ". "
+            << pl[i].fullname
+            << "\t" << pl[i].street
+            << "\t" << pl[i].numberHouse
+            << "\t\t"
+            << pl[i].numberApartment
+            << "\t\t"
+            << pl[i].dateSettle << endl;
+    }
+}
+
+void PrintMenu(record*& pl, int& page, int& choise);
+
+void SlideFirstPage(record*& pl, int& page, int& choise)
+{
+    while (true)
+    {
+        system("cls");
+        page = 0;
+        PrintHeader();
+        PrintBody(pl, page);
+        PrintMenu(pl, page, choise);
+    }
+}
+
+void SlideLastPage(record*& pl, int& page, int& choise)
+{
+    while (true)
+    {
+        system("cls");
+        page = 199;
+        PrintHeader();
+        PrintBody(pl, page);
+        PrintMenu(pl, page, choise);
+    }
+}
+
+void FunkMenu(record*& pl, int& page, int& choise)
+{
+    switch (choise)
+    {
+    case 1:
+        while (true)
+        {
+            system("cls");
+            page++;
+            PrintHeader();
+            PrintBody(pl, page);
+            PrintMenu(pl, page, choise);
+        }
+    case 2: SlideFirstPage(pl, page, choise);
+    case 3: SlideLastPage(pl, page, choise);
+    case 4: exit(1);
+    }
+
+}
+
+void PrintMenu(record*& pl, int& page, int& choise)
+{
+    cout << "Menu: " << "|<1>:Next page| \t"
+         << "|<2>:First page| \t" 
+         << "|<3>:Last page| \t" 
+         << "|<4>:Close program| \t" 
+         << "|<5>:Sort|\t" << endl;
+    cin >> choise;
+    cout << endl;
+    if (choise >= 1 && choise <= 5)
+    {
+        FunkMenu(pl, page, choise);
+    }
+    else
+    {
+        cout << "ERROR: invalid value" << endl;
+        PrintMenu(pl, choise, choise);
+    }
+}
+
 int main()
 {
     system("color 2");
     record* pl = new record[4000];
-    setlocale(LC_CTYPE, "Russian");
+
+    int choise = 0;
+    int page = 0;
+
     ifstream database("testBase4.dat", ios::binary);
 
-    if (!database) {
-        cout << "Не удалось открыть файл" << endl;
+    if (database.is_open())
+    {
+        cout << "Database is open!" << endl;
+        cout << endl;
+    }
+    else if (!database)
+    {
+        cout << "Database is not open" << endl;
         return 1;
     }
 
-    if (database.is_open()) {
-        cout << "Удалось открыть файл!" << endl;
-        cout << endl;
-        cout << "ФИО гражданина" << "\t\t\t" << "Название улицы" << "\t\t" << "Номер дома" << "\t" << "Номер квартиры" << "\t" << "Дата поселения" << endl;
-        cout << endl;
-        for (int i = 0; !database.read((char*)&pl[i], sizeof(record)).eof(); i++) {
-            cout << pl[i].fullname << "\t" << pl[i].street << "\t" << pl[i].numberHouse << "\t\t" <<
-                pl[i].numberApartment << "\t\t" << pl[i].dateSettle << endl;
-        }
+    for (int i = 0; !database.read((char*)&pl[i], sizeof(record)).eof(); i++);
+
+    while (true)
+    {
+        system("cls");
+        PrintHeader();
+        PrintBody(pl, page);
+        PrintMenu(pl, page, choise);
     }
 
     database.close();
